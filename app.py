e68605d38f90c5edd914a241627a2032e4235986
sobiaawan <sobiaawan56999@gmail.com>
2025-09-17 04:32:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import google.generativeai as genai
 
 # Configure Gemini with your API key from secrets
-genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+genai.configure(api_key=os.environ["AIzaSyBlq6_yZZgQUJHR6i97T-bflcggVLZbQkE"])
 
 st.title("Study Mode Clone 📚")
 
@@ -11,9 +11,10 @@ st.title("Study Mode Clone 📚")
 user_input = st.text_input("Ask me anything:")
 
 if user_input:
-    model = genai.GenerativeModel("gemini-pro")
+    model = genai.GenerativeModel("gemini 1.5 flash")
     response = model.generate_content(user_input)
 
     st.write("**Answer:**")
+
 
 
